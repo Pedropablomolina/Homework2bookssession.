f360fd51ab3e613ac3282325a342535d57cba643
pedropablomolina <ppmolinaarol@gmail.com>
2023-05-09 16:43:42 +0200

    Homework

--- hwsession12.py
+++ hwsession12.py
@@ -1,29 +1,36 @@
-import nltk
-from nltk.corpus import stopwords
+# Load the text files
+with open('book1.txt', 'r') as f:
+    book1 = f.read()
+with open('book2.txt', 'r') as f:
+    book2 = f.read()
 
-# Load and preprocess the text files
-book1 = open('book1.txt', 'r').read()
-book2 = open('book2.txt', 'r').read()
+# Define a function to preprocess the text
+def preprocess(text):
+    # Remove unwanted characters and punctuation
+    text = text.lower()
+    for char in '.,:;?!()[]{}-–"—\'\"':
+        text = text.replace(char, ' ')
+    # Split the text into words
+    words = text.split()
+    # Remove stopwords
+    stopwords = set(['the', 'and', 'of', 'to', 'in', 'a', 'that', 'it', 'with', 'is', 'was', 'for', 'as', 'on', 'by', 'at', 'an', 'be', 'this', 'which', 'not', 'are', 'from'])
+    words = [word for word in words if word not in stopwords]
+    return words
 
-stop_words = set(stopwords.words('english'))
-
-# Remove unwanted characters and stopwords
-book1_tokens = nltk.word_tokenize(book1.lower())
-book1_tokens = [word for word in book1_tokens if word.isalpha() and word not in stop_words]
-
-book2_tokens = nltk.word_tokenize(book2.lower())
-book2_tokens = [word for word in book2_tokens if word.isalpha() and word not in stop_words]
+# Preprocess the text
+book1_words = preprocess(book1)
+book2_words = preprocess(book2)
 
 # Calculate unique word count
-book1_unique_words = set(book1_tokens)
-book2_unique_words = set(book2_tokens)
+book1_unique_words = set(book1_words)
+book2_unique_words = set(book2_words)
 
 num_unique_words_book1 = len(book1_unique_words)
 num_unique_words_book2 = len(book2_unique_words)
 
 # Calculate total word count
-num_words_book1 = len(book1_tokens)
-num_words_book2 = len(book2_tokens)
+num_words_book1 = len(book1_words)
+num_words_book2 = len(book2_words)
 
 # Calculate unique to total word ratio
 unique_to_total_ratio_book1 = num_unique_words_book1 / num_words_book1
@@ -39,4 +46,3 @@ if unique_to_total_ratio_book1 > unique_to_total_ratio_book2:
     print("Book 1 has a higher ratio of unique to total words")
 else:
     print("Book 2 has a higher ratio of unique to total words")
-
